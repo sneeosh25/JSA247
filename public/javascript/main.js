86f5356e0231a2823d1f1c6498cc415257449ea9
Tyler Strand <tylermstrand@rescomp-13-276732.stanford.edu>
2014-05-05 22:55:21 -0700

    new chat working? test on live

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,247 +1,44 @@
+//Main.js
 
 
-
-// // Initial code by Borui Wang, updated by Graham Roth
-// // For CS247, Spring 2014
-
-// (function() {
-
-//   var cur_video_blob = null;
-//   var fb_instance;
-//   var msg_count = 0;
-
-//   $(document).ready(function(){
-//     connect_to_chat_firebase();
-//     connect_webcam();
-//   });
-
-//   function connect_to_chat_firebase(){
-//     /* Include your Firebase link here!*/
-//     fb_instance = new Firebase("https://cs247-p3-strand.firebaseio.com");
-
-//     // generate new chatroom id or use existing id
-//     var url_segments = document.location.href.split("/#");
-//     if(url_segments[1]){
-//       fb_chat_room_id = url_segments[1];
-//     }else{
-//       fb_chat_room_id = Math.random().toString(36).substring(7);
-//     }
-//     display_msg({m:"Share this url with your friend to join this chat: "+ document.location.origin+"/#"+fb_chat_room_id,c:"red"})
-//     display_msg({m:"Messages with emoticons will be animated and come with video.",c:"red"});
-//     display_msg({m:"To see an old video replayed, hover over the message. Click the video to close.",c:"red"});
-//     display_msg({m:"",c:"red"});
-
-//     // set up variables to access firebase data structure
-//     var fb_new_chat_room = fb_instance.child('chatrooms').child(fb_chat_room_id);
-//     var fb_instance_users = fb_new_chat_room.child('users');
-//     var fb_instance_stream = fb_new_chat_room.child('stream');
-//     var my_color = "#"+((1<<24)*Math.random()|0).toString(16);
-
-//     // listen to events
-//     fb_instance_users.on("child_added",function(snapshot){
-//       display_msg({m:snapshot.val().name+" joined the room",c: snapshot.val().c});
-//     });
-//     fb_instance_stream.on("child_added",function(snapshot){
-//       display_msg(snapshot.val());
-//     });
-
-//     // block until username is answered
-//     var username = window.prompt("Please declare your name?");
-//     if(!username){
-//       username = "anonymous"+Math.floor(Math.random()*1111);
-//     }
-//     fb_instance_users.push({ name: username,c: my_color});
-//     $("#waiting").remove();
-
-//     // bind submission box
-//     $("#submission input").keydown(function( event ) {
-//       if (event.which == 13) {
-//         var emotion = has_emotions($(this).val());
-//         if(emotion){
-//           fb_instance_stream.push({m:username+": " +$(this).val(), v:cur_video_blob, c: my_color});
-//         }else{
-//           fb_instance_stream.push({m:username+": " +$(this).val(), c: my_color});
-//         }
-//         $(this).val("");
-//       }
-//     });
-//   }
-
-//   // creates a message node and appends it to the conversation
-//   function display_msg(data){
-//     msg_count++;
-//     $(".hover_vid").hide();
-//     if(data.v){
-//       // for video element
-//       type_message(data, msg_count);
-
-//       var video = document.createElement("video");
-//       video.id = "vid_"+msg_count;
-//       video.className = "hover_vid";
-//       video.autoplay = true;
-//       video.controls = false; // optional
-//       video.loop = true;
-//       video.width = 120;
-
-//       var source = document.createElement("source");
-//       source.src =  URL.createObjectURL(base64_to_blob(data.v));
-//       source.type =  "video/webm";
-
-//       video.appendChild(source);
-      
-//       // for gif instead, use this code below and change mediaRecorder.mimeType in onMediaSuccess below
-//       // var video = document.createElement("img");
-//       // video.src = URL.createObjectURL(base64_to_blob(data.v));
-
-//       document.getElementById("conversation").appendChild(video);
-//       $("#msg_"+msg_count).hover(function() {
-//         $this = $(this);
-//         var id = $this.attr('id').substring(4);
-//         var vid = $("#vid_"+id);
-//         vid.fadeIn();
-//         //vid.left = "0px";
-//         //vid.top = "0px";
-//       });
-//       $("#vid_"+msg_count).click(function() {
-//         $this = $(this);
-//         var id = $this.attr('id').substring(4);
-//         var vid = $("#vid_"+id);
-//         vid.fadeOut();
-//       });
-//     } else {
-//       $("#conversation").append("<div class='msg' style='color:"+data.c+"' id='msg_"+msg_count+"'>"+data.m+"</div>");
-//     }
-//     // Scroll to the bottom every time we display a new message
-//     scroll_to_bottom(0);
-//   }
-
-//   var showText = function (target, msg, index, interval) {
-//     console.log(msg.length);
-//     if (index < msg.length) {
-//       $(target).append(msg[index++]);
-//       setTimeout(function () { showText(target, msg, index, interval); }, interval);
-//     }
-//   }
-
-//   function type_message(data, msg_count) {
-//     $("#conversation").append("<div class='vid_msg' style='color:"+data.c+"' id='msg_"+msg_count+"'></div>");
-//     var start = data.m.indexOf(": ");
-//     $("#msg_"+msg_count).append(data.m.substring(0, start + 1));
-//     var target = "#msg_"+msg_count; 
-//     showText(target, data.m, start + 1, 50)
-//     // $("#msg_"+msg_count).append(data.m[i]);
-//   }
-
-//   function scroll_to_bottom(wait_time){
-//     // scroll to bottom of div
-//     setTimeout(function(){
-//       $("html, body").animate({ scrollTop: $(document).height() }, 200);
-//     },wait_time);
-//   }
-
-//   function connect_webcam(){
-//     // we're only recording video, not audio
-//     var mediaConstraints = {
-//       video: true,
-//       audio: false
-//     };
-
-//     // callback for when we get video stream from user.
-//     var onMediaSuccess = function(stream) {
-//       // create video element, attach webcam stream to video element
-//       var video_width= 160;
-//       var video_height= 120;
-//       var webcam_stream = document.getElementById('webcam_stream');
-//       var video = document.createElement('video');
-//       webcam_stream.innerHTML = "";
-//       // adds these properties to the video
-//       video = mergeProps(video, {
-//           controls: false,
-//           width: video_width,
-//           height: video_height,
-//           src: URL.createObjectURL(stream)
-//       });
-//       video.play();
-//       webcam_stream.appendChild(video);
-
-//       // counter
-//       var time = 0;
-//       var second_counter = document.getElementById('second_counter');
-//       var second_counter_update = setInterval(function(){
-//         second_counter.innerHTML = time++;
-//       },1000);
-
-//       // now record stream in 5 seconds interval
-//       var video_container = document.getElementById('video_container');
-//       var mediaRecorder = new MediaStreamRecorder(stream);
-//       var index = 1;
-
-//       mediaRecorder.mimeType = 'video/webm';
-//       // mediaRecorder.mimeType = 'image/gif';
-//       // make recorded media smaller to save some traffic (80 * 60 pixels, 3*24 frames)
-//       mediaRecorder.video_width = video_width/2;
-//       mediaRecorder.video_height = video_height/2;
-
-//       mediaRecorder.ondataavailable = function (blob) {
-//           //console.log("new data available!");
-//           video_container.innerHTML = "";
-
-//           // convert data into base 64 blocks
-//           blob_to_base64(blob,function(b64_data){
-//             cur_video_blob = b64_data;
-//           });
-//       };
-//       setInterval( function() {
-//         mediaRecorder.stop();
-//         mediaRecorder.start(3000);
-//       }, 3000 );
-//       console.log("connect to media stream!");
-//     }
-
-//     // callback if there is an error when we try and get the video stream
-//     var onMediaError = function(e) {
-//       console.error('media error', e);
-//     }
-
-//     // get video stream from user. see https://github.com/streamproc/MediaStreamRecorder
-//     navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
-//   }
-
-//   // check to see if a message qualifies to be replaced with video.
-//   var has_emotions = function(msg){
-//     var options = ["lol",":)",":("];
-//     for(var i=0;i<options.length;i++){
-//       if(msg.indexOf(options[i])!= -1){
-//         return options[i];
-//       }
-//     }
-//     return false;
-//   }
+var apiKey = "44754112";
+var sessionId = "1_MX40NDc1NDExMn5-TW9uIE1heSAwNSAyMjo0Njo1NiBQRFQgMjAxNH4wLjE3MjQ1MjU3flB-";
+var subToken = "T1==cGFydG5lcl9pZD00NDc1NDExMiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz1lYWI3NDc2ZWY0NDhhMjQ0ODFmM2M1OGVmZDc3ZGUzMGUxMjAxNDNlOnJvbGU9c3Vic2NyaWJlciZzZXNzaW9uX2lkPTFfTVg0ME5EYzFOREV4TW41LVRXOXVJRTFoZVNBd05TQXlNam8wTmpvMU5pQlFSRlFnTWpBeE5INHdMakUzTWpRMU1qVTNmbEItJmNyZWF0ZV90aW1lPTEzOTkzNTUyMzMmbm9uY2U9MC43MzE0MDMxOTMyNTkxMzAyJmV4cGlyZV90aW1lPTE0MDE5NDcyMTAmY29ubmVjdGlvbl9kYXRhPQ==";
+var pubToken = "T1==cGFydG5lcl9pZD00NDc1NDExMiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz1mMTA4NGFkNGQ2YWFiNjBiOGFmZDc1OTFlZmI5ZWQyNDhmNTQ0NDA2OnJvbGU9cHVibGlzaGVyJnNlc3Npb25faWQ9MV9NWDQwTkRjMU5ERXhNbjUtVFc5dUlFMWhlU0F3TlNBeU1qbzBOam8xTmlCUVJGUWdNakF4Tkg0d0xqRTNNalExTWpVM2ZsQi0mY3JlYXRlX3RpbWU9MTM5OTM1NTI0NiZub25jZT0wLjQ2MzAzOTY3MTM3NjQ0Mjc1JmV4cGlyZV90aW1lPTE0MDE5NDcyMTAmY29ubmVjdGlvbl9kYXRhPQ==";
+var modToken = "T1==cGFydG5lcl9pZD00NDc1NDExMiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz1mYzQxNTBkM2RlYTFlN2Q1MGRhM2ZkYWRhYWM2MzgxNTczZTI5YzRkOnJvbGU9bW9kZXJhdG9yJnNlc3Npb25faWQ9MV9NWDQwTkRjMU5ERXhNbjUtVFc5dUlFMWhlU0F3TlNBeU1qbzBOam8xTmlCUVJGUWdNakF4Tkg0d0xqRTNNalExTWpVM2ZsQi0mY3JlYXRlX3RpbWU9MTM5OTM1NTI2OCZub25jZT0wLjM1NzMzNTkxNTAwOTkwOSZleHBpcmVfdGltZT0xNDAxOTQ3MjEwJmNvbm5lY3Rpb25fZGF0YT0=";
 
 
-//   // some handy methods for converting blob to base 64 and vice versa
-//   // for performance bench mark, please refer to http://jsperf.com/blob-base64-conversion/5
-//   // note useing String.fromCharCode.apply can cause callstack error
-//   var blob_to_base64 = function(blob, callback) {
-//     var reader = new FileReader();
-//     reader.onload = function() {
-//       var dataUrl = reader.result;
-//       var base64 = dataUrl.split(',')[1];
-//       callback(base64);
-//     };
-//     reader.readAsDataURL(blob);
-//   };
+$(document).ready(function(){
+  initialize();
+});
 
-//   var base64_to_blob = function(base64) {
-//     var binary = atob(base64);
-//     var len = binary.length;
-//     var buffer = new ArrayBuffer(len);
-//     var view = new Uint8Array(buffer);
-//     for (var i = 0; i < len; i++) {
-//       view[i] = binary.charCodeAt(i);
-//     }
-//     var blob = new Blob([view]);
-//     return blob;
-//   };
+function initialize() {
+  $("#initSession").click(initSession);
+}
 
-// })();
+
+function initSession() {
+  console.log("initializing session");
+
+  var session = OT.initSession(apiKey, sessionId);
+
+  // session.on("sessionConnected", function(event) {
+  //   console.log("session connected");
+  //   var publisher = OT.initPublisher();
+  //   session.publish(publisher);
+  // });
+
+  session.on("streamCreated", function(event) {
+    console.log("stream created");
+    session.subscribe(event.stream);
+  });
+
+  session.connect(pubToken, function(error) {
+    console.log("connect");
+    var publisher = OT.initPublisher();
+    session.publish(publisher);
+  });
+
+}
+
+
